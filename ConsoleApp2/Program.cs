39b459a2623530304b08bd5b5fd168009789c4e3
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:02:18 +0800

    練習一

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,6 +7,17 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            double meal_cost = 100;
+            int tip_percent = 15;
+            int tax_percent = 8;
+            int meal_costint = (int)meal_cost;
+            int sum = meal_costint + tip_percent + tax_percent;
+            Console.WriteLine($"用餐總費用:" + sum);
+
+
         }
+
+
     }
 }
